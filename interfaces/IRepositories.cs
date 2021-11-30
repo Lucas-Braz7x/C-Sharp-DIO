94147ea3a3bbbf768d112557eba65b0b2abf40aa
Lucas-Braz7x <lucasdutrabraz@gmail.com>
2021-11-29 22:07:07 -0300

    Create interface for 'genero'

---
+++ interfaces/IRepositories.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+namespace Cadastro_series.interfaces
+{
+  public interface IRepositories<T>
+  {
+    List<T> Lista();
+    T RetornaPorId(int id);
+    void Insere(T entidade);
+    void Exclui(int id);
+    void Atualiza(int id, T entidade);
+    int proximoId();
+  }
+}
