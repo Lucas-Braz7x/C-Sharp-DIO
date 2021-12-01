a350e2b354f51797d4caf0bb43744c35c4ffe634
Lucas-Braz7x <lucasdutrabraz@gmail.com>
2021-12-01 20:43:57 -0300

    Connection SQL Server

---
+++ app-web/Models/Categoria.cs
@@ -0,0 +1,9 @@
+
+namespace app_web.Models
+{
+  public class Categoria
+  {
+    public int id { get; set; }
+    public string descricao { get; set; }
+  }
+}
---
+++ app-web/Models/Context.cs
@@ -0,0 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace app_web.Models
+{
+  public class Context : DbContext
+  {
+    /*Criando uma tabela Categorias*/
+    public DbSet<Categoria> Categorias { get; set; }
+    /*Dentro deste método que eu informo o BD que vou utilizar*/
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      //DESKTOP-3PLJOV1
+      //"Data Source=MSSQL1;Initial Catalog=AdventureWorks; "Integrated Security=true;" (ESTE FUNCIONOU)
+      //@"Server=NT Service\MSSQLSERVER;Database=app_web;Integrated Security=True"
+      optionsBuilder.UseSqlServer(connectionString: "Data Source=DESKTOP-3PLJOV1;Initial Catalog=AdventureWorks; Integrated Security=true;");
+    }
+  }
+}
